c360cde3986fc1ad85565486f7c56ad6b9a3c067
Marijo Simunovic <simunovic.marijo@gmail.com>
2022-09-07 23:58:29 +0200

    Add in_degree, out_degree and page rank features for nodes

---
+++ src/graph/construct_features.py
@@ -0,0 +1,59 @@
+"""
+Adds additional features to the graph based on config
+"""
+from typing import Sequence, Set
+
+import networkx as nx
+import numpy as np
+from scipy import sparse
+from typeguard import typechecked
+
+from graph.construct_graph import DbGraphType
+
+
+def supported_digraph_features() -> Set[str]:
+    return {'ln', 'kc', 'in_degree', 'out_degree', 'pr_1', 'pr_2', 'pr_3', 'pr_4', 'pr_5'}
+
+
+def add_digraph_features(g, features) -> nx.DiGraph:
+    if 'in_degree' in features:
+        in_degrees = dict(g.in_degree())
+        nx.set_node_attributes(g, in_degrees, 'in_degree')
+    if 'out_degree' in features:
+        out_degrees = dict(g.out_degree())
+        nx.set_node_attributes(g, out_degrees, 'out_degree')
+
+    for feature in features:
+        # TODO: Does it makes sense to do this with torch instead and use GPU?
+        if feature.startswith('pr_'):
+            # get number of hops
+            k = int(feature.split('_')[1])
+            assert k > 0 and k < 6, f'Invalid number of hops {k}'
+            # get adjacency matrix
+            n = g.number_of_nodes()
+            A = nx.linalg.adjacency_matrix(g)
+            D = A.sum(axis=1)
+            Dinv = 1./ (D+1e-9); Dinv[D<1e-9] = 0 # take care of nodes without outgoing edges
+            Dinv = sparse.diags(np.squeeze(np.asarray(Dinv)), dtype=float) # D^-1 
+            P = (Dinv @ A).T 
+            pk_0 = np.ones(n) / n
+            x = pk_0.copy()
+            alpha = 0.95 
+            for k_idx in range(k):
+                x = alpha * P.dot(x) + (1-alpha) * pk_0
+                attr = {node: x[idx] for idx, node in enumerate(g.nodes)}
+                nx.set_node_attributes(g, attr, f'pr_{k_idx+1}')
+    
+    return g
+
+
+@typechecked
+def add_features(g: DbGraphType, features: Sequence[str]) -> DbGraphType:
+    if isinstance(g, nx.MultiDiGraph):
+        raise ValueError('Features are not yet supported for multigraphs')
+    elif isinstance(g, nx.DiGraph):
+        assert all(feat in supported_digraph_features() for feat in features)
+        return add_digraph_features(g, features)
+        # assert supported features
+    else:
+        raise ValueError(f'Unknown graph type {type(g)}')
---
+++ test/unit/graph/test_construct_features.py
@@ -0,0 +1,68 @@
+# TODO:
+# test that the order (i.e. adjacency operations are OK) is preserved
+# test with real data
+import pytest
+
+import networkx as nx
+from omegaconf import OmegaConf
+
+from graph.construct_graph import construct_graph
+from graph.construct_features import add_features
+
+
+@pytest.fixture
+def simple_digraph() -> nx.DiGraph:
+    # nodes = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+    edges = [(0, 1), (0, 2), (1, 3), (1, 4), (2, 5), (2, 6), (3, 7), (4, 7), (5, 8), (6, 8)]
+    return nx.DiGraph(incoming_graph_data=edges)
+
+
+def test_add_features_in_degree_out_degree(simple_digraph):
+    g = add_features(simple_digraph, ['in_degree', 'out_degree'])
+    expected_in_degrees = dict(g.in_degree())
+    expected_out_degrees = dict(g.out_degree())
+    for node_with_data in g.nodes(data=True):
+        nid, attrs = node_with_data
+        expected_in_degree = expected_in_degrees[nid]
+        expected_out_degree = expected_out_degrees[nid]
+        assert expected_in_degree == attrs['in_degree']
+        assert expected_out_degree == attrs['out_degree']
+
+
+def test_add_features_pr(simple_digraph):
+    g = add_features(simple_digraph, ['pr_1'])
+    expected_pr1 = {0: 0.005555, 1: 0.058333, 2: 0.058333, 3: 0.058333, 4: 0.058333, 5: 0.058333, 6: 0.058333, 7: 0.216666, 8: 0.216666}
+    for node_with_data in g.nodes(data=True):
+        nid, attrs = node_with_data
+        assert pytest.approx(expected_pr1[nid], rel=1e-3) == attrs['pr_1']
+
+    g = add_features(simple_digraph, ['pr_4'])
+    for node_with_data in g.nodes(data=True):
+        _, attrs = node_with_data
+        assert 'pr_1' in attrs
+        assert 'pr_2' in attrs
+        assert 'pr_3' in attrs
+        assert 'pr_4' in attrs
+
+
+#@pytest.mark.parametrize('graph_type', ['digraph', 'multigraph'])
+def test_add_features_lja_graph(test_gfa_root): #, graph_type):
+    graph_type = 'digraph'
+    cfg = OmegaConf.create({
+        'graph_type': graph_type, 
+        'gfa_path': test_gfa_root / 'lja_graph.gfa',
+        'k': 501
+    })
+
+    g = construct_graph(cfg)
+    g = add_features(g, features=['ln', 'kc', 'pr_4', 'in_degree', 'out_degree'])
+    for node_with_data in g.nodes(data=True):
+        _, attrs = node_with_data
+        assert 'ln' in attrs
+        assert 'kc' in attrs
+        assert 'pr_1' in attrs
+        assert 'pr_2' in attrs
+        assert 'pr_3' in attrs
+        assert 'pr_4' in attrs
+        assert 'in_degree' in attrs
+        assert 'out_degree' in attrs
