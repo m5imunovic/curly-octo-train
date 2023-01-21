df003ffb963776c6115386c42285f5886d4ba891
Marijo Simunovic <simunovic.marijo@gmail.com>
2023-01-21 16:36:59 +0100

    Add option to execute full lja on the given input

--- test/unit/asm/test_la_jolla.py
+++ test/unit/asm/test_la_jolla.py
@@ -1,11 +1,13 @@
 import tempfile
 from pathlib import Path
 
+import omegaconf
+
 from asm import la_jolla
 
 
 def test_la_jolla_construct_exe_cmd(test_reads_root):
-    cfg1 = {
+    cfg1 = omegaconf.DictConfig({
         'name': 'LJA',
         'params': {
             'long': {
@@ -15,8 +17,10 @@ def test_la_jolla_construct_exe_cmd(test_reads_root):
                 'k': 501
             }, 
             'append':  '--compress'
-        }
-    }
+        },
+        'exec': 'jumboDBG',
+        'full_asm': 'full_asm',
+    })
 
     with tempfile.TemporaryDirectory() as tmp_dir:
         vendor_dir = Path(tmp_dir) / 'vendor'
@@ -27,12 +31,18 @@ def test_la_jolla_construct_exe_cmd(test_reads_root):
         lja = la_jolla.LaJolla(cfg1, vendor_dir)
         reads_path = test_reads_root / 'chr1'
         output_path = Path(tmp_dir) / 'output'
-        cmd = lja._construct_exec_cmd(reads_path, output_path)
+        reads = [reads_path / '0_0001.fastq', reads_path / '1_0001.fastq']
+        cmd = lja._construct_exec_cmd(reads_files=reads, ref_path=reads_path, output_path=output_path)
 
-        assert len(cmd) == 1
-        print(cmd)
+        assert len(cmd) == 2
 
-        assert str(assembler_root / 'bin/lja') in cmd[0]
-        assert '-k 501 --threads 8 --compress' in cmd[0]
+        assert str(assembler_root / 'bin/jumboDBG') in cmd[0]
+        assert '-k 501' in cmd[0]
+        assert '--threads 8' in cmd[0]
+        assert '--compress' in cmd[0]
+        assert '--reference' in cmd[0]
         assert str(reads_path / '0_0001.fastq') in cmd[0]
         assert str(reads_path / '1_0001.fastq') in cmd[0]
+        assert str(assembler_root / 'bin/lja') in cmd[1]
+        assert '--compress' not in cmd[1]
+        assert '--reference' not in cmd[1]
