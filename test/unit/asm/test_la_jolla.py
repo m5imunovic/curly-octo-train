a73e54a49241a02b56454ee8d8e262f88d882b5c
Marijo Simunovic <simunovic.marijo@gmail.com>
2023-02-09 23:27:28 +0100

    Postpone lja eval by storing necessary command inputs in file

--- test/unit/asm/test_la_jolla.py
+++ test/unit/asm/test_la_jolla.py
@@ -1,4 +1,5 @@
 import tempfile
+from unittest.mock import patch
 from pathlib import Path
 
 import omegaconf
@@ -32,12 +33,13 @@ def test_la_jolla_construct_exe_cmd(test_reads_root):
         reads_path = test_reads_root / 'chr1'
         output_path = Path(tmp_dir) / 'output'
         reads = [reads_path / '0_0001.fastq', reads_path / '1_0001.fastq']
-        cmd = lja._construct_exec_cmd(reads_files=reads, ref_path=reads_path, output_path=output_path)
+        with patch("json.dump") as mock_dump, patch("builtins.open") as mock_open:
+            cmd = lja._construct_exec_cmd(reads_files=reads, ref_path=reads_path, output_path=output_path)
 
         # call jumboDBG
         # call lja
         # call align_and_print twice
-        assert len(cmd) == 4
+        assert len(cmd) == 1
 
         assert str(assembler_root / 'bin/jumboDBG') in cmd[0]
         assert '-k 501' in cmd[0]
@@ -46,6 +48,6 @@ def test_la_jolla_construct_exe_cmd(test_reads_root):
         assert '--reference' in cmd[0]
         assert str(reads_path / '0_0001.fastq') in cmd[0]
         assert str(reads_path / '1_0001.fastq') in cmd[0]
-        assert str(assembler_root / 'bin/lja') in cmd[1]
-        assert '--compress' not in cmd[1]
-        assert '--reference' not in cmd[1]
+        assert "full_asm.json" in str(mock_open.call_args_list[0])
+        assert "lja" in mock_dump.call_args[0][0]
+        assert "align_and_print" in mock_dump.call_args[0][0]
