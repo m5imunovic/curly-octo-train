7ed1e8c387d44f1a498522365c9d8935c6cb9bcd
Marijo Simunovic <simunovic.marijo@gmail.com>
2023-04-30 11:34:31 +0200

    Update read extraction scripts

--- scripts/map_reads.sh
+++ scripts/map_reads.sh
@@ -1,29 +1,23 @@
 #!/bin/bash
 
-# mamba install -c bioconda sra-tools
-# mamba install -c bioconda minimap2
-# mamba install -c bioconda seq
+# mamba install -c bioconda sra-tools minimap2 seqkit
 
-SRR_ENTRY="SRR9087597"
+# SRR_ENTRY="SRR9087597"
+SRR_ENTRY="SRX5633451"
 
-mkdir fasta
 mkdir sra
+# unpack sra file
+mkdir fasta
+mkdir fastq
+# output sra entry to fasta directory using non-wrapped reads (i.e. 1-line == entire read)
 fastq-dump -O fasta/ --fasta 0 sra/"$SRR_ENTRY"
-minimap2 -x map-hifi -H -t 20 -d ref.mmi references/chm13_v2.0/chm13v2.0.fa.gz
-minimap2 -x map-hifi -H -c -t 20 ref.mmi fasta/"$SRR_ENTRY" > paf/alignment_"$SRR_ENTRY".paf
 
-CHR=18
-TARGET_ID=chr"$CHR"
-# assumes tab delimited otherwise cut -d'<delimiter>'
+
 mkdir paf
-grep "$TARGET_ID" paf/"$SRR_ENTRY"_alignment.paf | cut -f 1 > paf/"$TARGET_ID"_reads.idx
-
-# -f index file containing sequence ids - used as pattern
-# -o output file name
-# -w 0 - do not wrap
-
-OUTPUT_DIR=reads/chr_"$CHR_real"/XX_XX/SXX/
-mkdir -p $OUTPUT_DIR
-seqkit grep -f paf/"$TARGET_ID"_reads.idx fasta/"$SRR_ENTRY".fasta -o $OUTPUT_DIR/"$TARGET_ID"_0001.fasta -w 0
-
+REF_DIR=references/chm13_v2.0
+minimap2 -x map-hifi -H -t 20 -d "$REF_DIR"/ref.mmi "$REF_DIR"/chm13v2.0.fa.gz
+# Fasta
+# minimap2 -x map-hifi -H -c -t 20 "$REF_DIR"/ref.mmi fasta/"$SRR_ENTRY" > paf/alignment_"$SRR_ENTRY".paf
+# Fastq
+minimap2 -x map-hifi -H -c -t 20 "$REF_DIR"/ref.mmi fastq/"$SRR_ENTRY".fastq > paf/alignment_"$SRR_ENTRY".paf
 
