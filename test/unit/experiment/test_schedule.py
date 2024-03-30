4d5411d562b43a4fe88f45aa7c1bca0d91e3e0e6
m5imunovic <simunovic.marijo@gmail.com>
2024-03-30 18:00:13 +0100

    Add idmap to kept files

--- test/unit/experiment/test_schedule.py
+++ test/unit/experiment/test_schedule.py
@@ -12,18 +12,18 @@ def test_create_sequencing_jobs(tmpdir, test_scenario_sim, test_data_reference):
 
     job = jobs[0]
     assert job["genome"][0].name == "chr1.fasta"
-    assert job["seed"] == 10001
+    assert job["seed"] == 30001
     assert job["output_path"].relative_to(staging_root) == Path("0/reads")
 
     job = jobs[1]
     assert job["genome"][0].name == "chr1.fasta"
-    assert job["seed"] == 10002
+    assert job["seed"] == 30002
     assert job["output_path"].relative_to(staging_root) == Path("1/reads")
 
     job = jobs[2]
     assert job["genome"][0].name == "chr1.fasta"
     assert job["genome"][1].name == "chr2.fasta"
-    assert job["seed"] == 10001
+    assert job["seed"] == 30001
     assert job["output_path"].relative_to(staging_root) == Path("2/reads")
 
 
@@ -38,7 +38,14 @@ def test_schedule_run_sim_creates_expected_outputs(
         run(test_experiment_sim_cfg)
 
     assert (Path(tmpdir) / "unittest_dataset").exists()
-    assert len(list((Path(tmpdir) / "unittest_dataset" / "train").glob("**/*.pt"))) == 3
+    assert len(list((Path(tmpdir) / "unittest_dataset" / "test").glob("**/*.pt"))) == 3
+    eval_path = Path(tmpdir) / "unittest_dataset" / "eval"
+    assert eval_path.exists()
+    for sample_idx in ["0", "1", "2"]:
+        # do hard-coded checking, actually should be cross-checked with experiment.keep option
+        assert eval_path / sample_idx / "reads" / "sim_0001.fastq"
+        assert eval_path / sample_idx / "assemblies" / "mult.info"
+        assert eval_path / sample_idx / "graph" / "debug" / "0.idmap"
 
 
 def test_schedule_run_sample_creates_expected_outputs(
