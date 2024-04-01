a69472209c99f7c0c1d48a14fee58d227890b7d3
m5imunovic <simunovic.marijo@gmail.com>
2024-04-01 12:17:41 +0200

    Restore gfa parsing with new interface

---
+++ test/unit/graph/tools/test_gfa_parser.py
@@ -0,0 +1,25 @@
+import pytest
+
+from graph.tools.gfa_parser import parse_gfa
+
+
+def test_parse_lja_gfa(test_graph_root):
+    example_gfa = test_graph_root / "gfa" / "final_dbg.gfa"
+
+    segments, links = parse_gfa(example_gfa, k=501)
+
+    assert len(links) == 0
+    assert len(segments) == 10
+    print(segments)
+
+    assert "-1535.1" in segments.keys()
+    assert "1871.2" in segments.keys()
+
+    segment = segments["-1535.1"]
+    assert "hash" in segment
+    assert "kc" in segment
+    assert "ln" in segment
+
+    assert segment["ln"] == 1957
+    assert pytest.approx(1.0, rel=1e-6) == segment["kc"]
+    assert segment["hash"] == "1824157992947779194109204387820039613390C"
